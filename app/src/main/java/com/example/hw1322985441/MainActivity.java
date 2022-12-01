ee703342a84f1c75b42b7254737112b132b842f8
orielmalik <oriel.malik@gmail.com>
2022-12-02 00:17:16 +0200

    final commit

--- app/src/main/java/com/example/hw1322985441/MainActivity.java
+++ app/src/main/java/com/example/hw1322985441/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.hw1322985441;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,28 +15,34 @@ public class MainActivity extends AppCompatActivity  {
     private ExtendedFloatingActionButton efl, efr;
     private ImageView h1, h2, h3, cruise, c2, c3;//c2-left
     private ImageView[][] rocks;
-private static  int b=0;
-private TimerActivity timer;
+    private int[][]arr;// to attach th ImagView Matrix
+    private int[] place;//place of matrix
+     private static  int b=0;
+    private TimerActivity timer;
+public static  int choice=0;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+this.arr=new int[3][3];
+this.place=new int[3];
+this.rocks=new ImageView[3][3];
+startMatrix();
+this.timer=new TimerActivity(this.rocks,this.arr);
 
-this.timer=new TimerActivity();
-this.timer.onCreate(savedInstanceState);
-      this.timer.startTimer(this.rocks);
 
+this.timer.startTimer();
 
 
         cruise = new ImageView(this);
         c2 = new ImageView(this);
         c3 = new ImageView(this);
         findViews();
-       // visi(c2, c3);
-       // startMatrix();
+        visi(c2, c3);
 
-     //   rockFalls();
         MakeActiotoLeft();
         MakeActiotoRight();
     }
@@ -54,30 +63,82 @@ this.timer.onCreate(savedInstanceState);
     private void MakeActiotoLeft() {
         efr.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                visi(cruise, c3);
-                timer.stopTimer();
+                if(choice==2)
+                {
+                    visi(c2, c3);
+                    cruise.setVisibility(View.VISIBLE);
+                    choice--;
+
+                }
+                else {
+                    visi(cruise, c3);
+                    c2.setVisibility(View.VISIBLE);
+                    choice = 1;
+                    timer.stopTimer();
+                }
             }
         });
+    }
+
+
+    private void clicked(boolean answer) {
+        Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+
+    }
+private void  AttachCruiseToGame()
+{
+if(choice==1)
+{
+    this.place[0]=1;
+}
+
+else  if(choice==2)
+{
+    this.place[2]=1;
+
+}
+else
+{
+    this.place[1]=1;
+}
+
+
+
+}
+
+    private void toast() {
+
+        Toast
+                .makeText(this," GoodBye",Toast.LENGTH_LONG)
+                .show();
     }
 
     private void MakeActiotoRight() {
         efl.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                visi(cruise, c3);
-b=0;
+                if(choice==1)
+                {
+                    visi(c2, c3);
+                    cruise.setVisibility(View.VISIBLE);
+                    choice++;
+
+                }
+                else {
+                    visi(cruise, c2);
+                    c3.setVisibility(View.VISIBLE);
+                    toast();
+                    choice = 2;
+                }
 
             }
         });
     }
 
-    private void visi(ImageView a, ImageView v) {
+    private void visi(ImageView a, ImageView v) {//to prevent duplicate code
         a.setVisibility(View.INVISIBLE);
         v.setVisibility(View.INVISIBLE);
 
     }
-
-
-
 
 
 
@@ -89,9 +150,9 @@ b=0;
             }
         }
         this.rocks[0][0].findViewById(R.id.r1);
-        this.rocks[0][1].findViewById(R.id.r2);
+        this.rocks[1][0].findViewById(R.id.r2);
         this.rocks[2][0].findViewById(R.id.r3);
-        this.rocks[1][0].findViewById(R.id.r4);
+        this.rocks[0][1].findViewById(R.id.r4);
         this.rocks[1][1].findViewById(R.id.r5);
         this.rocks[1][2].findViewById(R.id.r6);
         this.rocks[0][2].findViewById(R.id.r7);
@@ -100,7 +161,11 @@ b=0;
 
 
 
-
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                this.rocks[i][j].setVisibility(View.VISIBLE);
+            }
+        }
 
 
 
--- app/src/main/java/com/example/hw1322985441/TimerActivity.java
+++ app/src/main/java/com/example/hw1322985441/TimerActivity.java
@@ -1,6 +1,7 @@
 package com.example.hw1322985441;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
@@ -8,76 +9,126 @@ import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Random;
-import java.util.Timer;
-import java.util.TimerTask;
 
-public class TimerActivity extends AppCompatActivity {
+public  class TimerActivity extends AppCompatActivity {
+
     final int DELAY = 1000;
 
-long startTime=0;
+   int[][]rocks;
+    ImageView[][]target;
+
+    long startTime = 0;
+private  View vv;
+
+public  TimerActivity( ImageView[][]tar,int[][] a)
+{
+    //this.rocks=new int[3][3];
+
+  //  this.target=new ImageView[3][3];
+    this.target=tar;
+    this.rocks=a;
+}
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-    }
-        public void updateTimerUI() {
-            long millis = System.currentTimeMillis() - startTime;
-            int seconds = (int) (millis / 1000);
-            int minutes = seconds / 60;
-            seconds = seconds % 60;
-            int hours = minutes / 60;
-            minutes = minutes % 60;
-            hours %= 24;
-
-        }
-
-    public void stopTimer() {
-        timer.cancel();
-    }
-
-    private Timer timer = new Timer();
-
-
-    private void rockFalls(ImageView[][]rocks) {
-
-        Random r = new Random();
-        int ra = r.nextInt(2);
-       rocks[2][ra].setVisibility(View.VISIBLE);
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (i != 2 && j != ra) {
-                   rocks[i][j].setVisibility(View.INVISIBLE);
-                }
+        final Handler handler = new Handler();
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                handler.postDelayed(this, DELAY); //do it again in a second.
+                updateRocks(rocks,3,3,target);
             }
+        };
+
+    }
+
+    final Handler handler = new Handler();
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            handler.postDelayed(this, DELAY); //do it again in a second.
+            updateRocks(rocks,3,3,target);
         }
+    };
 
+    private void updateTimerUI() {
+        Log.d("TimerCount", "" + System.currentTimeMillis());
+        long millis = System.currentTimeMillis() - startTime;
+        int seconds = (int) (millis / 1000);
+        int minutes = seconds / 60;
+        seconds = seconds % 60;
+        int hours = minutes / 60;
+        minutes = minutes % 60;
+        hours %= 24;
 
     }
 
+  private void helper(int[][] rocks,int c,int r) {
+      Random rs = new Random();
 
+      int ra = rs.nextInt(rocks[0].length - 1);
+     rs = new Random();
 
+      int rt = rs.nextInt(rocks.length - 1);
 
-    public void startTimer(ImageView[][]rocks) {
+      rocks[rt][ra] = 1;
+
+      for (int i = 0; i < r; i++) {
+          for (int j = 0; j < c; j++) {
+              if (i != rt && j != ra) {
+                  rocks[i][j] = 0;
+              }
+          }
+      }
+  }
+    public void stopTimer() {
+        handler.removeCallbacks(runnable);
+    }
+
+   public void startTimer() {
         startTime = System.currentTimeMillis();
-        timer = new Timer();
-        timer.scheduleAtFixedRate(
-                new TimerTask() {
-                    @Override
-                    public void run() {
-                        Log.d("TimerCount A:", Thread.currentThread().getName() + " " + System.currentTimeMillis());
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                rockFalls(rocks);
-                                Log.d("TimerCount B:", Thread.currentThread().getName() + " " + System.currentTimeMillis());
-
-                            }
-                        });
-                    }
-                }
-                , DELAY, DELAY);
-    }
-
-
+        handler.postDelayed(runnable,DELAY);
 
     }
+
+              private void updateRocks(int[][] rocks, int c, int r, ImageView[][] target)
+              {
+                  helper(rocks,c,r);
+                  for (int i = 0; i < r; i++) {
+                      for (int j = 0; j < c; j++) {
+
+                          if(rocks[i][j]==1)
+                          {
+                          target[i][j].setVisibility(View.VISIBLE);
+                          }
+                          else
+                          {
+                              target[i][j].setVisibility(View.INVISIBLE);
+
+                          }
+                  }
+                      }
+
+              }
+
+
+    public void setRocks(int[][] rocks) {
+        this.rocks = rocks;
+    }
+
+    public int[][] getRocks() {
+        return rocks;
+    }
+
+    public void setTarget(ImageView[][] target) {
+        this.target = target;
+    }
+
+    public ImageView[][] getTarget() {
+        return target;
+    }
+}
